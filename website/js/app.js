1ab22878909a200790e8a502dabf3c1550c120a4
marwan osama <mo989989@gmail.com>
2021-12-30 06:38:46 +0200

    add loading and saving background images

--- website/js/app.js
+++ website/js/app.js
@@ -1,22 +1,43 @@
 const inputDataForm = document.getElementById("input-location-form");
+const tempReportWrap = document.querySelector("#temp-report-wrapper");
 const tempDisplay = document.getElementById("temp");
 const tempHeading = document.getElementById('temp-report-heading');
 const tempMore = document.querySelector('#temp-more');
 const generateButton = document.getElementById("generate-button");
-const contentBackgroundClouds = document.getElementById("clouds-background");
-const contentBackgroundRain = document.getElementById("rain-background");
-const contentBackgroundClear = document.getElementById("clear-background");
-const contentBackgroundMist = document.getElementById("mist-background");
-const contentBackgroundHaze = document.getElementById("haze-background");
+const backgrounds = {
+    rain: $("#rain-background"),
+    clear: $("#clear-background"),
+    mist: $("#mist-background"),
+    haze: $("#haze-background"),
+    snow: $("#snow-background"),
+    clouds: $("#clouds-background")
+}
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
 const apiKey = '180bd9f9a2698eb069f24a0d48ea1833';
 
+
+function Loading() {
+    this.id = `loading-${Math.floor(Math.random() * 1000)}`
+    this.loadingHtml = `<div class='loader' id=${this.id}></div>`;
+    this.fragment;
+
+    this.show = function(parent) {
+        this.element = document.createElement("div");
+        this.element.classList.add("loader");
+        this.element.id = this.id;
+        parent.appendChild(this.element);
+    }
+    this.hide = function() {
+        document.querySelector(`#${this.id}`).remove();
+    }
+}
+
+
 const resetBackground = function() {
-    $(contentBackgroundClear).hide();
-    $(contentBackgroundRain).hide();
-    $(contentBackgroundClouds).hide();
-    $(contentBackgroundMist).hide();
+    for (let i in backgrounds) {
+        backgrounds[i].hide();
+    }
 }
 
 const getWeatherData = async function(city) {
@@ -32,22 +53,13 @@ const getWeatherData = async function(city) {
 }
 const updateTemp = async function(weatherData) {
     if (weatherData.cod == 200) {
-        tempHeading.innerHTML = `Temperature right now in ${weatherData.name} is:`
+        tempHeading.innerHTML = `Temperature right now in ${weatherData.name}, ${weatherData.sys.country} is:`
         tempDisplay.innerHTML = `${weatherData.main.temp}&#176;`;
         tempMore.innerHTML = `<p id="temp-max">Max. temp: ${weatherData.main.temp_max}&#176;</p><p id="temp-min">Min. temp: ${weatherData.main.temp_min}&#176;</p><p id="weather">Weather: ${weatherData.weather[0].main}</p>`;
 
         resetBackground();
-        if (weatherData.weather[0].main == "Clouds") {
-            $(contentBackgroundClouds).fadeIn(250);
-        } else if (weatherData.weather[0].main == "Rain") {
-            $(contentBackgroundRain).fadeIn(250);
-        } else if (weatherData.weather[0].main == "Clear") {
-            $(contentBackgroundClear).fadeIn(250);
-        } else if (weatherData.weather[0].main == "Mist") {
-            $(contentBackgroundMist).fadeIn(250);
-        } else if (weatherData.weather[0].main == "Haze") {
-            $(contentBackgroundHaze).fadeIn(250);
-        }
+        backgrounds[weatherData.weather[0].main.toLowerCase()].fadeIn(250);
+
     } else 
     {
         tempHeading.innerHTML = "invalid city";
@@ -57,10 +69,21 @@ const updateTemp = async function(weatherData) {
 }
 const handleFormSubmit = function(event) {
     event.preventDefault();
+    requestData();
+}
+
+const requestData = function() {
     const city = document.getElementById("city-input").value;
+    const loader = new Loading();
+    const tempReport = $("#temp-report");
+    tempReport.hide();
+    loader.show(tempReportWrap);
+    
     if (city) {
         getWeatherData(city).then(
             function(weatherData) {
+                loader.hide();
+                tempReport.show();
                 updateTemp(weatherData);
             }
         )
@@ -104,8 +127,5 @@ const updateUI = async function(data) {
 inputDataForm.onsubmit = handleFormSubmit;
 inputDataForm.oninput = function () {getJson().then(function(data){updateUI(data)})};
 
-getWeatherData("new york").then(
-    function(weatherData) {
-        updateTemp(weatherData);
-    }
-)
+document.getElementById("city-input").value = "new york";
+requestData();
